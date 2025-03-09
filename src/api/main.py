3d54d31456ce8e91932d917080e1717f6962bef4
NhiNguyenAI <nhintnguyenAI@gmail.com>
2025-03-09 11:55:08 +0100

    Added the read file as image to read the image

--- src/api/main.py
+++ src/api/main.py
@@ -1,23 +1,35 @@
 from fastapi import FastAPI, File, UploadFile
 from uvicorn import run
 from PIL import Image
-import io
+from io import BytesIO
+import numpy as np
 
 app = FastAPI()
 
+# ----------------------------------------------------------------------------------------------------------------
+# Functions
+# ----------------------------------------------------------------------------------------------------------------
+def read_file_as_image(data) -> np.ndarray:
+    image = np.Array(Image.open(BytesIO(data)))
+    return image
+# ----------------------------------------------------------------------------------------------------------------
+# API
+# ----------------------------------------------------------------------------------------------------------------
+
+
 @app.get("/")
 async def root():
-    return {"message": "Welcome to the Potato Disease Classification API!"}
-
-@app.get("/ping")
-async def ping():
-    return {"message": "Pong!"}
+    return {"Welcome to the Potato Disease Classification API!"}
 
 @app.post("/predict")
 async def predict(
     file: UploadFile = File(...)
-    ):
-    pass
+):
+    # Use wait and async to read the file asynchronously, when don't use async, the file will be read synchronously
+    # If 100 users upload files at the same time, the server will read the files one by one. If using async, the server will read the files at the same time
+    images = read_file_as_image(await file.read())
+
+    return
 
 if __name__ == "__main__":
     run(app, host="localhost", port=8000)
