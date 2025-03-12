78f6865096bd6b60260b4c25e3f7e9e7ec19007d
NhiNguyenAI <nhintnguyenAI@gmail.com>
2025-03-12 15:58:03 +0100

    added the frontend for this project

--- src/api/main.py
+++ src/api/main.py
@@ -4,8 +4,22 @@ from PIL import Image
 from io import BytesIO
 import numpy as np
 import tensorflow as tf
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+
+origins = [
+    "http://localhost:3000",  # React frontend
+    "http://localhost",  # FastAPI backend (if needed)
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # ----------------------------------------------------------------------------------------------------------------
 # Functions
@@ -34,38 +48,29 @@ async def root():
 
 # Predict endpoint
 @app.post("/predict")
-async def predict(
-    file: UploadFile = File(...)  # Ensure the field name is "file"
-):
+async def predict(file: UploadFile = File(...)):
     try:
-        # Read the image file asynchronously
         image = read_file_as_image(await file.read())
-        
-        # Check the image shape
         print("Image shape:", image.shape)
         
-        # Add a batch dimension (batch_size = 1)
         image_batch = np.expand_dims(image, axis=0)
         print("Image batch shape:", image_batch.shape)
         
-        # Get predictions from the model
         predictions = MODEL.predict(image_batch)
         print("Predictions:", predictions)
         
-        # Get the predicted class and confidence
         predicted_class_index = np.argmax(predictions[0])
         predicted_class = CLASS_NAMES[predicted_class_index]
         confidence = np.max(predictions[0])
         
         return {
-            "prediction_class": predicted_class,
-            "confidence": float(confidence)  # Convert to float for JSON response
+            "class": predicted_class,  
+            "confidence": float(confidence)
         }
     except Exception as e:
-        # Log the error and return a meaningful message
         print(f"Error processing the file: {e}")
         return {"error": "Failed to process the image. Please ensure the file is a valid image."}
 
+    
 if __name__ == "__main__":
     run(app, host="localhost", port=8000)
-    
--- src/api/main_tf_serving.py
+++ src/api/main_tf_serving.py
@@ -9,6 +9,18 @@ import requests
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 # ----------------------------------------------------------------------------------------------------------------
 # Functions
 # ----------------------------------------------------------------------------------------------------------------
